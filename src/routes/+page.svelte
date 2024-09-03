2eb362938480958710846d4f3764d36b8ea304cd
mixrecovery19 <michael.kalaf@gmail.com>
2024-09-03 16:35:01 +1000

    Styling edits

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,7 @@
     };
 </script>
 <h1 class="text-center">MixRecovery19 Recipe Library</h1>
+<p class="home-ptag">Welcome to the MixRecovery19 Recipe Library</p>
 <p class="home-ptag">Go To Recipes To Search For Recipes</p>
 <p class="home-ptag">Use Member Submissions To Create and Share Your Own</p>
 <button on:click={navigateToRecipes}>
